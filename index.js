f0f781635066a84b170ba92425fe84795f32c262
Tyler Hogue <hogue73@embarqmail.com>
2024-12-05 15:06:12 -0500

    passed tests

---
+++ index.js
@@ -0,0 +1,10 @@
+let answer
+
+const divide = function(number1, number2){
+answer = number1 / number2
+return answer
+}
+
+const square = number => number ** 2
+
+const add = (number1, number2) => number1 + number2
